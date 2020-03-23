c378b99b2455987792970b742c6666aa4b1cf0fe
karan11singla <karan.singla@hbo.com>
2020-03-22 18:04:51 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Events from './Events';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to UVolunteer</h2>
+        </div>
+        <Events> 
+      </div>
+    );
+  }
+}
+
+export default App;
