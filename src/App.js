c99c5ab28223d4c36e03ed5b0ee22a1fb194eb05
karan11singla <karan.singla@hbo.com>
2020-03-25 09:46:41 -0700

    add map

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Self-Report Covid-19 CoronaVirus</h2>
         </div>
+       <div class="embed-container">
+       <iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Coronavirus - India" src="//akiboy96.maps.arcgis.com/apps/Embed/index.html?webmap=733b02909c9a486fb06cc772c14b32e1&extent=-180,-73.418,180,81.0989&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light"></iframe></div>
       </div>
     );
   }
