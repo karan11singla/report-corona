69f6d4ac929022e98781f7ebfd53a7c1f8971ebf
karan11singla <karan.singla@hbo.com>
2020-03-25 10:09:51 -0700

    add legends

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h2>Self-Report Covid-19 CoronaVirus</h2>
         </div>
        <div class="embed-container">
-       <iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Coronavirus - India" src="//akiboy96.maps.arcgis.com/apps/Embed/index.html?webmap=733b02909c9a486fb06cc772c14b32e1&extent=-180,-73.418,180,81.0989&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light"></iframe></div>
+       <iframe width="400" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Coronavirus - India" src="//akiboy96.maps.arcgis.com/apps/Embed/index.html?webmap=733b02909c9a486fb06cc772c14b32e1&extent=-180,-72.7433,180,81.4506&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legend=true&basemap_gallery=true&disable_scroll=true&theme=dark"></iframe></div>
       </div>
     );
   }
