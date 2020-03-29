a84bb2cc971172245ef19f91337ba76cf3337c3e
karan11singla <karan.singla@hbo.com>
2020-03-29 15:39:42 -0700

    import events

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ class App extends Component {
           <h2>Self-Report Covid-20 CoronaVirus</h2>
         </div>
        <div class="embed-container">
-       <iframe width="400" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Coronavirus - India" src="//akiboy96.maps.arcgis.com/apps/Embed/index.html?webmap=733b02909c9a486fb06cc772c14b32e1&extent=-180,-72.7433,180,81.4506&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legend=true&basemap_gallery=true&disable_scroll=true&theme=dark"></iframe></div>  
-     <Button href="#">Link</Button> <Button type="submit">Button</Button>{' '}
-     <Button as="input" type="button" value="Input" />{' '}
-     <Button as="input" type="submit" value="Submit" />{' '}
-     <Button as="input" type="reset" value="Reset" />
-     </div>
+       <Button href="#">Link</Button> <Button type="submit">Button</Button>{' '}
+       <Button as="input" type="button" value="Input" />{' '}
+       <Button as="input" type="submit" value="Submit" />{' '}
+       <Button as="input" type="reset" value="Reset" />
+       </div>
+    </div>
     );
   }
 }
