7bd797a9d06f8e7d3f9492250b4081c4f7fda148
nitish7892 <61034728+nitish7892@users.noreply.github.com>
2020-03-29 17:31:24 -0500

    Update
    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button} from 'react-bootstrap';
+import './Map.js'
+
 
 class App extends Component {
 
@@ -9,11 +12,15 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Self-Report Covid-19 CoronaVirus</h2>
+          <h2>Self-Report Covid-20 CoronaVirus</h2>
         </div>
        <div class="embed-container">
-       <iframe width="400" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Coronavirus - India" src="//akiboy96.maps.arcgis.com/apps/Embed/index.html?webmap=733b02909c9a486fb06cc772c14b32e1&extent=-180,-72.7433,180,81.4506&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legend=true&basemap_gallery=true&disable_scroll=true&theme=dark"></iframe></div>
-      </div>
+       <iframe width="400" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Coronavirus - India" src="//akiboy96.maps.arcgis.com/apps/Embed/index.html?webmap=733b02909c9a486fb06cc772c14b32e1&extent=-180,-72.7433,180,81.4506&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legend=true&basemap_gallery=true&disable_scroll=true&theme=dark"></iframe></div>  
+     <Button href="#">Link</Button> <Button type="submit">Button</Button>{' '}
+     <Button as="input" type="button" value="Input" />{' '}
+     <Button as="input" type="submit" value="Submit" />{' '}
+     <Button as="input" type="reset" value="Reset" />
+     </div>
     );
   }
 }
