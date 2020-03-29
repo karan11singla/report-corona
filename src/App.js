f3255eca740bb2d292eedf0d775ce91962248168
nitish7892 <61034728+nitish7892@users.noreply.github.com>
2020-03-29 18:50:24 -0500

    update - Events.js file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button} from 'react-bootstrap';
-import './Map.js'
+import './Events.js'
 
 
 class App extends Component {
--- src/Events.js
+++ src/Events.js
@@ -4,7 +4,8 @@ class Events extends Component {
 	render() {
 
 		return(
-			<div>	<iframe width="400" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Coronavirus - India" src="//akiboy96.maps.arcgis.com/apps/Embed/index.html?webmap=733b02909c9a486fb06cc772c14b32e1&extent=-180,-72.7433,180,81.4506&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legend=true&basemap_gallery=true&disable_scroll=true&theme=dark"></iframe></div>  </div>
+		<div class="embed-container">	<iframe width="400" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Coronavirus - India" src="//akiboy96.maps.arcgis.com/apps/Embed/index.html?webmap=733b02909c9a486fb06cc772c14b32e1&extent=-180,-72.7433,180,81.4506&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legend=true&basemap_gallery=true&disable_scroll=true&theme=dark"></iframe></div> 
+
 		);
 	}
 }
