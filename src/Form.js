8b9e0ed83a957671f67d5d47fcf67a28d41b406b
nitish7892 <61034728+nitish7892@users.noreply.github.com>
2020-03-30 21:57:27 -0500

    form updates

--- src/Form.js
+++ src/Form.js
@@ -1,8 +1,51 @@
 import React, { Component }  from 'react';
+import { CountryDropdown, RegionDropdown, CountryRegionData } from 'react-country-region-selector';
+import DropdownTreeSelect from "react-dropdown-tree-select";
+import "./index.css";
+import data from "./data.json";
+
+
+const onChange = (currentNode, selectedNodes) => {
+  console.log("path::", currentNode.path);
+};
+
 
 export default function Form() {
-  return(
+
+
+  return (
+    <div className="App">
+    <p>Form</p>
+    <div>
+    <form >
+    <label>First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+
+    <label>Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+
+  <label>Email</label>
+    <input type="email" id="email" name="email" placeholder="Your email" />
+
+    <p>Enter country</p>
+    <label>Country</label>
+    <DropdownTreeSelect data={data} onChange={onChange} />
     
-  	<div> <h1> HelloW orld </h1></div>
-    );
+ 
+    <label>Address</label>
+    <input type="address" id="address" name="address" placeholder="Your address" />
+
+    <label>Symptoms</label>
+    <textarea id="symptoms" name="symptoms" placeholder="Comments.."></textarea>
+
+    <label>Corona(Yes?)</label>
+    <input type="radio" id="coronaflag" name="Coronaflag"  />
+
+
+    <input type="submit" value="Submit" />
+    </form>
+    </div>
+    </div>
+  );
 }
+
